6c7f8bd6bdf685afb672eed0faaae85722ece18d
kesylo <keming.loic@gmail.com>
2020-05-08 02:41:29 +0200

    added pm2

--- post-install.sh
+++ post-install.sh
@@ -7,7 +7,7 @@ PWD=$1
 echo "$PWD" | sudo -S apt-get update -y
 
 echo "$PWD" | curl -sL https://deb.nodesource.com/setup_12.x | sudo -E -S bash -
-echo "$PWD" | sudo -S apt-get update -y
+apt-get update -y
 
 # Check if node is installed
 which node > /dev/null 2>&1
@@ -15,7 +15,7 @@ if [ $? -eq 0 ]; then
     echo "Node is installed, skipping..."
 else
     echo "Node is NOT installed, installing..."
-    echo "$PWD" | sudo -S apt-get install nodejs -y
+    apt-get install nodejs -y
 fi
 
 # Check if pm2 is installed
@@ -23,7 +23,7 @@ which pm2 > /dev/null 2>&1
 if [ $? -eq 0 ]; then
     echo "PM2 is installed, skipping..."
     # stop pm2
-    echo "$PWD" | sudo -S pm2 restart server
+    pm2 restart server
 else
     echo "PM2 is NOT installed, installing..."
     npm i -g pm2
@@ -32,9 +32,10 @@ else
 fi
 
 # set as process on startup
+exit
 LOGGED_IN_USER=$(whoami)
-echo "$PWD" | sudo -S pm2 startup "$LOGGED_IN_USER"
+pm2 startup "$LOGGED_IN_USER"
 
 # display status
 pm2 status
-echo "$PWD" | sudo -S pm2 save
+pm2 save
